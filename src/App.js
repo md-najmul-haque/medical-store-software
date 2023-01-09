ef07311983b74f87aa3fe3832e8356f0ffd2dff1
Md Najmul Haque <najmulhaque008@gmail.com>
2023-01-09 18:00:35 +0600

    dashboard template created

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import ItemForm from './components/ItemForm';
 import CategoryDetails from './components/CategoryDetails';
 import CreateSalesBill from './components/CreateSalesBill';
 import AddNewPayment from './components/AddNewPayment';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
+      <Dashboard />
       {/* <ItemForm /> */}
       {/* <CategoryDetails /> */}
       {/* <CreateSalesBill /> */}
-      <AddNewPayment />
+      {/* <AddNewPayment /> */}
     </div>
   );
 }
