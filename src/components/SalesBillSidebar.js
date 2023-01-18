600929c1181dfb2c81f79af29c8e0c814b36f4f8
Md Najmul Haque <najmulhaque008@gmail.com>
2023-01-18 12:07:38 +0600

    search feature implemented

--- src/components/SalesBillSidebar.js
+++ src/components/SalesBillSidebar.js
@@ -4,12 +4,15 @@ import MedicineCard from './MedicineCard';
 
 const SalesBillSidebar = () => {
     const [medicines, setMedicines] = useState([])
+    const [query, setQuery] = useState('')
 
     useEffect(() => {
         fetch('medicines.json')
             .then(res => res.json()
                 .then(data => setMedicines(data)))
     }, [])
+
+    console.log(medicines.filter(medicine => medicine.name.toLowerCase().includes(query)))
 
     const handleAllMedicine = () => {
 
@@ -26,7 +29,7 @@ const SalesBillSidebar = () => {
                     <div class="flex justify-center">
                         <div class="mb-3 xl:w-96">
                             <div class="input-group relative flex flex-wrap items-stretch w-full mb-4">
-                                <input type="search" class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-success focus:outline-none" placeholder="Search" aria-label="Search" aria-describedby="button-addon2" />
+                                <input onChange={event => setQuery(event.target.value)} type="search" class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-success focus:outline-none" placeholder="Search" aria-label="Search" aria-describedby="button-addon2" />
                                 <button class="btn inline-block px-6 py-2.5 bg-success text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-success hover:shadow-lg focus:bg-success  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-success active:shadow-lg transition duration-150 ease-in-out flex items-center" type="button" id="button-addon2">
                                     <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                         <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
@@ -40,7 +43,7 @@ const SalesBillSidebar = () => {
                         <select className="select w-full input-bordered focus:outline-none">
                             <option disabled selected>Select Medicine</option>
                             {
-                                medicines.map(medicine => <option key={medicine._id}>{medicine.name}</option>)
+                                medicines.filter(medicine => medicine.name.toLowerCase().includes(query)).map(medicine => <option key={medicine._id}>{medicine.name}</option>)
                             }
 
                         </select>
@@ -48,7 +51,7 @@ const SalesBillSidebar = () => {
                 </div>
                 <div className='grid grid-cols-4'>
                     {
-                        medicines.map(medicine => <MedicineCard key={medicine._id} medicine={medicine} />)
+                        medicines.filter(medicine => medicine.name.toLowerCase().includes(query)).map(medicine => <MedicineCard key={medicine._id} medicine={medicine} />)
                     }
                 </div>
 
