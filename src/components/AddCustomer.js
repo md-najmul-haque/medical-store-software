1ab963d488fb651f0ca2f873fadea01656d97cf5
Md Najmul Haque <najmulhaque008@gmail.com>
2023-01-31 19:16:42 +0600

    add customer modal added

--- src/components/AddCustomer.js
+++ src/components/AddCustomer.js
@@ -30,62 +30,60 @@ const AddCustomer = ({ setAddCustomer }) => {
             <div className="modal w-full modal-bottom sm:modal-middle">
                 <div className="modal-box w-full bg-white">
                     <label htmlFor="add-customer" className="btn btn-sm btn-secondary text-white btn-square absolute right-3 top-3">✕</label>
-                    <h3 className="font-bold text-2xl text-primary"> Details about</h3>
-                    <div className="card mx-auto">
-
-                        <div className="card-body items-center text-center">
-                            <form onSubmit={handleSubmit(onSubmit)}>
-
-                                <div className="form-control w-full">
-                                    <label className="label">
-                                        <span className="label-text">Customer Name</span>
-                                    </label>
-                                    <input
-                                        type="text"
-                                        placeholder="Customer Name"
-                                        className="input bg-gray-100 w-full "
-                                        {...register("customerName", {
-                                            required: {
-                                                value: true,
-                                                message: 'Customer name is required'
-                                            }
-                                        })} />
-                                    <label className="label">
-                                        {errors.customerName?.type === 'required' && <span className="label-text-alt text-red-500">{errors.customerName.message}</span>}
-                                    </label>
-                                </div>
-
-                                <div className="form-control w-full">
-                                    <label className="label">
-                                        <span className="label-text">Phone Number</span>
-                                    </label>
-                                    <input
-                                        type="text"
-                                        placeholder="Phone Number"
-                                        className="input bg-gray-100 w-full"
-                                        {...register("phoneNumber", {
-                                            required: {
-                                                value: true,
-                                                message: 'Phone number is required'
-                                            }
-                                        })} />
-                                    <label className="label">
-                                        {errors.phoneNumber?.type === 'required' && <span className="label-text-alt text-red-500">{errors.phoneNumber.message}</span>}
-                                    </label>
-                                </div>
-
-                                <div className="card-actions items-center justify-between">
-                                    <button onClick={() => setAddCustomer(false)} className="btn btn-secondary text-white px-10">Close</button>
-                                    <input type="submit" className="btn btn-primary text-white px-10" value='Submit' />
-                                </div>
+                    <h3 className="font-bold text-2xl text-primary"> Add New Customer </h3>
 
 
+                    <div className="card-body p-3 items-center text-center mx-auto">
+                        <form onSubmit={handleSubmit(onSubmit)}>
 
-                            </form>
+                            <div className="form-control w-full">
+                                <label className="label">
+                                    <span className="label-text">Customer Name</span>
+                                </label>
+                                <input
+                                    type="text"
+                                    placeholder="Customer Name"
+                                    className="input bg-gray-100 w-full "
+                                    {...register("customerName", {
+                                        required: {
+                                            value: true,
+                                            message: 'Customer name is required'
+                                        }
+                                    })} />
+                                <label className="label">
+                                    {errors.customerName?.type === 'required' && <span className="label-text-alt text-red-500">{errors.customerName.message}</span>}
+                                </label>
+                            </div>
 
-                        </div>
+                            <div className="form-control w-full">
+                                <label className="label">
+                                    <span className="label-text">Phone Number</span>
+                                </label>
+                                <input
+                                    type="text"
+                                    placeholder="Phone Number"
+                                    className="input bg-gray-100 w-full"
+                                    {...register("phoneNumber", {
+                                        required: {
+                                            value: true,
+                                            message: 'Phone number is required'
+                                        }
+                                    })} />
+                                <label className="label">
+                                    {errors.phoneNumber?.type === 'required' && <span className="label-text-alt text-red-500">{errors.phoneNumber.message}</span>}
+                                </label>
+                            </div>
+
+                            <div className="card-actions items-center justify-between">
+                                <button onClick={() => setAddCustomer(false)} className="btn btn-secondary text-white px-10">Close</button>
+                                <input type="submit" className="btn btn-primary text-white px-10" value='Submit' />
+                            </div>
+
+                        </form>
 
                     </div>
+
+
 
                 </div>
             </div >
