d2f94adeb295989c364b258025ed82f81f904f32
md-najmul-haque <najmulhaque008@gmail.com>
2023-02-05 17:06:42 +0600

    supplier list header completed

--- src/components/SupplierList.js
+++ src/components/SupplierList.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { IoIosCreate } from 'react-icons/io';
 
 
 const SupplierList = () => {
 
     return (
-        <div>
-
+        <div className='bg-white h-screen'>
+            <div className='bg-base-100 py-3 px-5 border-b-2 flex items-center'>
+                <h1 className='uppercase text-xl font-bold text-left'>Supplier List</h1>
+                <Link to='' className="btn btn-primary text-white normal-case btn-sm ml-5" ><span className="mr-1"><IoIosCreate /></span> Create Supplier</Link>
+            </div>
         </div>
     )
 }
