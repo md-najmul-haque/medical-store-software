564eb8d4ea69fb3c3a8c2ed20d8243fb758b490a
md-najmul-haque <najmulhaque008@gmail.com>
2023-02-05 17:32:13 +0600

    card added

--- src/components/SupplierList.js
+++ src/components/SupplierList.js
@@ -11,6 +11,29 @@ const SupplierList = () => {
                 <h1 className='uppercase text-xl font-bold text-left'>Supplier List</h1>
                 <Link to='' className="btn btn-primary text-white normal-case btn-sm ml-5" ><span className="mr-1"><IoIosCreate /></span> Create Supplier</Link>
             </div>
+            <div className="flex gap-16 justify-end mt-5 mr-2">
+                <div className="card w-96 p-0 bg-base-100 shadow-xl">
+                    <div className="card-body p-0">
+                        <h2 className="bg-secondary p-3 text-white">Total Payable(BDT)</h2>
+                        <p>55</p>
+                    </div>
+                </div>
+
+                <div className="card w-96 p-0 bg-base-100 shadow-xl">
+                    <div className="card-body p-0">
+                        <h2 className="bg-secondary p-3 text-white">Total Payable(BDT)</h2>
+                        <p>55</p>
+                    </div>
+                </div>
+
+                <div className="card w-96 p-0 bg-base-100 shadow-xl">
+                    <div className="card-body p-0">
+                        <h2 className="bg-secondary p-3 text-white">Total Payable(BDT)</h2>
+                        <p>55</p>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
