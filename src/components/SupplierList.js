29e980d189b43a38c7b29ba1ef533ab7b4147b9c
md-najmul-haque <najmulhaque008@gmail.com>
2023-02-05 22:54:07 +0600

    status added in supplier list

--- src/components/SupplierList.js
+++ src/components/SupplierList.js
@@ -130,7 +130,7 @@ const SupplierList = () => {
                                                         {supplier.payable - supplier.paid}
                                                     </td>
                                                     <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-
+                                                        {supplier.status}
                                                     </td>
                                                 </tr>
                                             )
