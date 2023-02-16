b9dada764261fbcc9402c3a56992c7dc519143df
md-najmul-haque <najmulhaque008@gmail.com>
2023-02-16 12:58:33 +0600

    import feature added in supplier list

--- src/components/Supplier/ImportSupplier.js
+++ src/components/Supplier/ImportSupplier.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import { useForm } from 'react-hook-form';
+import React, { useState } from 'react'
 import { BiDownload } from 'react-icons/bi';
-
+import Papa from 'papaparse';
 
 const ImportSupplier = ({ setImportSupplier }) => {
-    const { register, formState: { errors }, handleSubmit, reset } = useForm();
-    const onSubmit = data => {
-        console.log(data)
-        setImportSupplier(false)
+    const [supplierData, setSupplierData] = useState([])
+    console.log(supplierData)
 
+    function handleSupplier() {
+        // const file = event.target.files[0];
+        Papa.parse(supplierData, {
+            complete: function (results) {
+                console.log(results.data);
+                // do something with the parsed data
+            }
+        });
     }
 
     return (
@@ -22,22 +27,22 @@ const ImportSupplier = ({ setImportSupplier }) => {
                         <h1 className='uppercase text-left text-primary text-xl font-bold'>Import supplier CSV file</h1>
                     </div>
 
-                    <form className='mt-5' onSubmit={handleSubmit(onSubmit)}>
-                        <p className='font-semibold text-xl'>Download Sample supplier CSV file <button className='btn btn-primary text-white'> <span className='font-bold mr-1'><BiDownload /></span> Download</button></p>
 
-                        <div className='mb-12 mt-2'>
-                            <p className='font-semibold text-xl mb-2'>Select CSV file </p>
-                            <input type="file" className="file-input file-input-secondary  w-full max-w-xs" />
-                        </div>
+                    <p className='font-semibold text-xl'>Download Sample supplier CSV file <button className='btn btn-primary text-white'> <span className='font-bold mr-1'><BiDownload /></span> Download</button></p>
 
-                        <hr />
+                    <div className='mb-12 mt-2'>
+                        <p className='font-semibold text-xl mb-2'>Select CSV file </p>
+                        <input type="file" onChange={e => setSupplierData(e.target.files[0])} className="file-input file-input-secondary  w-full max-w-xs" />
+                    </div>
 
-                        <div className="flex justify-end items-center  mt-5">
-                            <button onClick={() => setImportSupplier(false)} className="btn btn-ghost bg-base-100 text-primary px-10 mr-5">Close</button>
-                            <input type="submit" className="btn btn-primary px-10 text-white" value='Upload' />
-                        </div>
+                    <hr />
 
-                    </form>
+                    <div className="flex justify-end items-center  mt-5">
+                        <button onClick={() => setImportSupplier(false)} className="btn btn-ghost bg-base-100 text-primary px-10 mr-5">Close</button>
+                        <input onClick={handleSupplier} type="submit" className="btn btn-primary px-10 text-white" value='Upload' />
+                    </div>
+
+
 
                 </div>
 
