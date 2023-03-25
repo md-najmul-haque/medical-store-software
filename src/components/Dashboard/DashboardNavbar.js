a239f8f8ade3bdc9f443236d0a1f75e1531e7a8b
Md Najmul Haque <najmulhaque008@gmail.com>
2023-03-25 13:13:51 +0600

    user logout feature added in the dashboard icon

--- src/components/Dashboard/DashboardNavbar.js
+++ src/components/Dashboard/DashboardNavbar.js
@@ -36,9 +36,22 @@ const DashboardNavbar = () => {
                     </div>
                 </button>
 
-                <button className="btn btn-ghost btn-circle text-2xl">
-                    < FaUserPlus />
-                </button>
+
+            </div>
+            <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                    <div className="w-10 rounded-full">
+                        <img src="https://i.ibb.co/85v9KGV/Portrait-Placeholder.png" />
+                    </div>
+                </label>
+                <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                    <li>
+                        <a className="justify-between">
+                            Profile
+                        </a>
+                    </li>
+                    <li><a>Logout</a></li>
+                </ul>
             </div>
         </div>
 
