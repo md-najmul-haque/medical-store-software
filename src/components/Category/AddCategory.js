75f0bfdf746ce360c154430c50cf23cb3aea0ace
Md Najmul Haque <najmulhaque008@gmail.com>
2023-04-01 16:19:25 +0600

    add category updated

--- src/components/Category/AddCategory.js
+++ src/components/Category/AddCategory.js
@@ -36,6 +36,8 @@ const AddCategory = ({ setCategoryModal }) => {
                         status: data.status,
                     }
 
+                    console.log(category)
+
                     fetch(`http://localhost:5000/api/v1/category`, {
                         method: "POST",
                         body: JSON.stringify(category),
@@ -69,91 +71,91 @@ const AddCategory = ({ setCategoryModal }) => {
                     </div>
 
                     <form onSubmit={handleSubmit(onSubmit)}>
-                        <div className=''>
-                            <div className='grid grid-cols-3 gap-x-5 pl-5 pr-20'>
-                                <div className="form-control w-full col-span-2">
-                                    <label className="label">
-                                        <span className="label-text">CAT ID</span>
-                                    </label>
-                                    <input
-                                        type="number"
-                                        placeholder="Enter CAT ID"
-                                        className="input bg-gray-100 w-full input-bordered"
-                                        {...register("catId", {
-                                            required: {
-                                                value: true,
-                                                message: 'CAT ID is required'
-                                            }
-                                        })} />
-                                    <label className="label">
-                                        {errors.catId?.type === 'required' && <span className="label-text-alt text-red-500">{errors.catId.message}</span>}
-                                    </label>
-                                </div>
 
-                                <div className="form-control w-full ">
-                                    <label className="label">
-                                        <span className="label-text">Status</span>
-                                    </label>
-                                    <select className="select w-full input-bordered" {...register("status")}>
-                                        <option selected>Active</option>
-                                        <option>Inactive</option>
-                                    </select>
-                                </div>
-
-                                <div className="form-control w-full col-span-2">
-                                    <label className="label">
-                                        <span className="label-text">Category Name</span>
-                                    </label>
-                                    <input
-                                        type="text"
-                                        placeholder="Category Name"
-                                        className="input w-full input-bordered"
-                                        {...register("categoryName", {
-                                            required: {
-                                                value: true,
-                                                message: 'Category name is required'
-                                            }
-                                        })} />
-                                    <label className="label">
-                                        {errors.categoryName?.type === 'required' && <span className="label-text-alt text-red-500">{errors.categoryName.message}</span>}
-                                    </label>
-                                </div>
-
-                                <div className="form-control w-full col-span-2">
-                                    <label className="label">
-                                        <span className="font-semibold">Add Category Image</span>
-                                    </label>
-                                    <input
-                                        type="file"
-                                        className="input bg-gray-100 w-full "
-                                        {...register("image", {
-                                            required: {
-                                                value: true,
-                                                message: 'Category image is required'
-                                            }
-                                        })} />
-                                    <label className="label">
-                                        {errors.image?.type === 'required' && <span className="label-text-alt text-red-500">{errors.image.message}</span>}
-                                    </label>
-                                </div>
-
-
-                            </div>
-
-                            <div className="form-control w-full col-span-2 ml-5 mt-10">
+                        <div className='grid grid-cols-3 gap-x-5 pl-5 pr-20'>
+                            <div className="form-control w-full col-span-2">
+                                <label className="label">
+                                    <span className="label-text">CAT ID</span>
+                                </label>
                                 <input
-                                    type="image"
-                                    alt=""
-                                    style={{ width: '200px', height: '200px' }}
-                                    className="input-bordered border-2 shadow-md"
-                                    {...register("image")} />
+                                    type="number"
+                                    placeholder="Enter CAT ID"
+                                    className="input bg-gray-100 w-full input-bordered"
+                                    {...register("catId", {
+                                        required: {
+                                            value: true,
+                                            message: 'CAT ID is required'
+                                        }
+                                    })} />
+                                <label className="label">
+                                    {errors.catId?.type === 'required' && <span className="label-text-alt text-red-500">{errors.catId.message}</span>}
+                                </label>
                             </div>
 
-                            <div className="flex justify-center items-center mt-5">
-                                <button onClick={() => setCategoryModal(false)} className="btn btn-secondary px-10 text-white mr-5">Close</button>
-                                <input type="submit" className="btn btn-primary px-10 text-white" value='Submit' />
+                            <div className="form-control w-full ">
+                                <label className="label">
+                                    <span className="label-text">Status</span>
+                                </label>
+                                <select className="select w-full input-bordered" {...register("status")}>
+                                    <option selected>Active</option>
+                                    <option>Inactive</option>
+                                </select>
                             </div>
+
+                            <div className="form-control w-full col-span-2">
+                                <label className="label">
+                                    <span className="label-text">Category Name</span>
+                                </label>
+                                <input
+                                    type="text"
+                                    placeholder="Category Name"
+                                    className="input w-full input-bordered"
+                                    {...register("categoryName", {
+                                        required: {
+                                            value: true,
+                                            message: 'Category name is required'
+                                        }
+                                    })} />
+                                <label className="label">
+                                    {errors.categoryName?.type === 'required' && <span className="label-text-alt text-red-500">{errors.categoryName.message}</span>}
+                                </label>
+                            </div>
+
+                            <div className="form-control w-full col-span-2">
+                                <label className="label">
+                                    <span className="font-semibold">Add Category Image</span>
+                                </label>
+                                <input
+                                    type="file"
+                                    className="input bg-gray-100 w-full "
+                                    {...register("image", {
+                                        required: {
+                                            value: true,
+                                            message: 'Category image is required'
+                                        }
+                                    })} />
+                                <label className="label">
+                                    {errors.image?.type === 'required' && <span className="label-text-alt text-red-500">{errors.image.message}</span>}
+                                </label>
+                            </div>
+
+
                         </div>
+
+                        {/* <div className="form-control w-full col-span-2 ml-5 mt-10">
+                            <input
+                                type="image"
+                                alt=""
+                                style={{ width: '200px', height: '200px' }}
+                                className="input-bordered border-2 shadow-md"
+                                {...register("image")} />
+                        </div> */}
+
+                        <div className="flex justify-center items-center mt-5">
+                            <button onClick={() => setCategoryModal(false)} className="btn btn-secondary px-10 text-white mr-5">Close</button>
+                            <input type="submit" className="btn btn-primary px-10 text-white" value='Submit' />
+                        </div>
+
                     </form>
                 </div>
 
