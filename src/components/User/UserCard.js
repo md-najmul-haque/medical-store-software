cc1ddfc9dc6f0bbe617986abe010f3b9f77f13d1
md-najmul-haque <najmulhaque008@gmail.com>
2023-04-02 18:25:42 +0600

    card drop down menu updated

--- src/components/User/UserCard.js
+++ src/components/User/UserCard.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BsThreeDotsVertical } from 'react-icons/bs';
 
 const UserCard = (user) => {
 
-    const { name, email, role } = user.user
+    const [open, setOpen] = useState(false)
+
+    const { name, email, role, image } = user.user
 
     console.log(user)
 
     return (
         <div className="card w-96 bg-white shadow-xl">
-            <figure className="px-10 pt-10">
-                <img src="" alt="Shoes" className="rounded-xl" />
-            </figure>
+
+            <div className='relative'>
+                <div className='flex justify-between px-7 pt-7'>
+                    <p>{role ? role : 'Stuff'}</p>
+                    <p onClick={() => setOpen(!open)}><BsThreeDotsVertical /></p>
+                </div>
+
+                <div className='absolute right-10' >
+                    {
+                        open ? <p>najmul</p> : ''
+                    }
+                </div>
+            </div>
+
+            <div className='flex justify-center item-center mt-10'>
+                <figure className="w-24">
+                    <img src={image ? image : "https://i.ibb.co/RQV7pPN/avatar.png"} alt="Shoes" className="rounded-full" />
+                </figure>
+            </div>
             <div className="card-body items-center text-center">
                 <h2 className="card-title">{name}</h2>
                 <p>{email}</p>
-                <p>{role ? role : ''}</p>
-                <div className="card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
-                </div>
+
             </div>
         </div>
     );
